a8a73b33ca9f10f3aa5754c6f39e10ca662e044b
vivek81094 <sffseed123@gmail.com>
2024-05-16 22:35:30 +0530

    contentupdate

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -8,8 +8,8 @@ const About = () => {
       <div className={styles.content}>
         
       <h1 className={styles.title}>About <span className={styles.color}>Me</span></h1>
-      <h2 className={styles.heading}>Front-End Developer</h2>
-      <p className={styles.description}> I am a dynamic front-end developer with a strong foundation in graphic design, boasting two years of experience. My creativity and professional skills shine through in my work, making me a valuable asset to any team. With two successful freelancing projects under my belt, I've demonstrated my ability to craft visually appealing and responsive websites that prioritize user experience. I'm passionate about my craft and deeply committed to optimizing website performance and ensuring accessibility for all users. My project management expertise and collaborative approach further enhance my reputation as a skilled and dedicated front-end developer.</p>
+      <h2 className={styles.heading}>Full-Stack Developer</h2>
+      <p className={styles.description}> I am a dynamic Full-Stack Developer with a strong foundation. My creativity and professional skills shine through in my work, making me a valuable asset to any team. With two successful freelancing projects under my belt, I've demonstrated my ability to craft visually appealing and responsive websites that prioritize user experience. I'm passionate about my craft and deeply committed to optimizing website performance and ensuring accessibility for all users. My project management expertise and collaborative approach further enhance my reputation as a skilled and dedicated front-end developer.</p>
 
       </div>
     </section>
